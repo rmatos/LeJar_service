2dd342a7f1c476fbcab70db98a52d137ced88366
Rudy E. Matos Perez <rudy.e.matos@gmail.com>
2015-10-28 00:25:17 -0400

    sending automatic emails from the app using heroku scheduler

---
+++ automated_tasks/app_automated_processes.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+function changeLeJarMode(){
+
+	console.log(process.env.EMAIL_USERNAME);
+	console.log(process.env.EMAIL_PASSWORD);
+
+	var transporter = nodemailer.createTransport({
+		service : 'Gmail',
+		auth:{
+			user : process.env.EMAIL_USERNAME,
+			pass : process.env.EMAIL_PASSWORD
+		}
+	});
+	var mailOptions = {
+		from 	: 'le-jar-service@lejar-service.noreply.com',
+		to 	 	: 'rmena28@gmail.com,rudy.e.matos@gmail.com,rudy12118@gmail.com',
+		subject : 'Testing Heroku Scheduler',
+		html 	: '<b> This is a test from Heroku Running an scheduler <b/>'
+	};
+	transporter.sendMail(mailOptions,function(error, info){
+		if(error) return console.log(error);
+		console.log('Message sent succesfully with message : '+info.response);
+	});
+}
+
+changeLeJarMode();
